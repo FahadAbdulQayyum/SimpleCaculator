20241d6f68a2a8723c71e967d6155011a5800caf
FahadAbdulQayyum <fahad.45843@iqra.edu.pk>
2023-08-14 20:38:56 +0500

    ui changed the colors

--- src/App.js
+++ src/App.js
@@ -58,27 +58,33 @@ function App() {
   
   return (
     <div>
-      <div className="w-[50%] bg-slate-500 m-auto h-screen flex flex-col gap-2 p-5">
-        <div className="bg-cyan-300 h-[80px] w-full rounded-full relative">
-        <input type='number' disabled value={+toCalculate>0 ? toCalculate.toString() : valueC.join("")} className="bg-slate-200 h-[80%] w-[97%] rounded-full m-2 outline-0 p-3" />
+      {/* <div className="w-[50%] bg-slate-500 m-auto h-screen flex flex-col gap-2 p-5"> */}
+      <div className="w-[50%] bg-gradient-to-b from-zinc-500 m-auto h-screen flex flex-col gap-2 p-5">
+        {/* <div className="bg-cyan-300 h-[80px] w-full rounded-full relative"> */}
+        {/* <div className="bg-gradient-to-r from-zinc-300 to-blue-500 h-[80px] w-full rounded-full relative"> */}
+        <div className="bg-gradient-to-r from-zinc-300 to-blue-500 h-[80px] w-full rounded-xl relative">
+        {/* <input type='number' disabled value={+toCalculate>0 ? toCalculate.toString() : valueC.join("")} className="bg-slate-200 h-[80%] w-[97%] rounded-full m-2 outline-0 p-3" /> */}
+        <input type='number' disabled value={+toCalculate>0 ? toCalculate.toString() : valueC.join("")} className="bg-slate-200 h-[80%] w-[97%] rounded-xl m-2 outline-0 p-3 text-4xl" />
         </div>
-        <div className="bg-cyan-300 h-[90%] w-full rounded place-content-center">
-            <div className="grid grid-cols-4 gap-4 p-2">
-              <button onClick={onPressBtn} value={1} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">1</button>
-              <button onClick={onPressBtn} value={2} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">2</button>
-              <button onClick={onPressBtn} value={3} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">3</button>
-              <button onClick={onPressBtn} value={'+'} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">+</button>
-              <button onClick={onPressBtn} value={4} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">4</button>
-              <button onClick={onPressBtn} value={5} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">5</button>
-              <button onClick={onPressBtn} value={6} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">6</button>
-              <button onClick={onPressBtn} value={'-'} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">-</button>
-              <button onClick={onPressBtn} value={7} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">7</button>
-              <button onClick={onPressBtn} value={8} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">8</button>
-              <button onClick={onPressBtn} value={9} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">9</button>
-              <button onClick={onPressBtn} value={'/'} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">/</button>
-              <button onClick={onPressBtn} value={'0'} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">0</button>
-              <button onClick={onPressBtn} value={'='} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">=</button>
-              <button onClick={onPressBtn} value={'x'} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">x</button>
+        {/* <div className="bg-cyan-300 h-[90%] w-full rounded place-content-center"> */}
+        <div className="bg-zinc-300 h-[90%] w-full rounded place-content-center">
+            <div className="grid grid-cols-4 gap-4 p-5">
+              {/* <button onClick={onPressBtn} value={1} className="bg-orange-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">1</button> */}
+              <button onClick={onPressBtn} value={1} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">1</button>
+              <button onClick={onPressBtn} value={2} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">2</button>
+              <button onClick={onPressBtn} value={3} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">3</button>
+              <button onClick={onPressBtn} value={'+'} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl">+</button>
+              <button onClick={onPressBtn} value={4} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">4</button>
+              <button onClick={onPressBtn} value={5} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">5</button>
+              <button onClick={onPressBtn} value={6} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">6</button>
+              <button onClick={onPressBtn} value={'-'} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl">-</button>
+              <button onClick={onPressBtn} value={7} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">7</button>
+              <button onClick={onPressBtn} value={8} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">8</button>
+              <button onClick={onPressBtn} value={9} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">9</button>
+              <button onClick={onPressBtn} value={'/'} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl">/</button>
+              <button onClick={onPressBtn} value={'0'} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl text-white">0</button>
+              <button onClick={onPressBtn} value={'='} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl">=</button>
+              <button onClick={onPressBtn} value={'x'} className="bg-gradient-to-r from-teal-500 to-blue-400 shadow drop-shadow-xl p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-4xl">x</button>
               <button onClick={onPressBtn} value={'C'} className="bg-red-400 p-5 text-center rounded active:scale-110 ease-in-out duration-300 text-lg font-bold">C</button>
           </div>
         </div>
